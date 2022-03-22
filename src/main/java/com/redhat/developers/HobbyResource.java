810a9b47dc4729d2e1e1078eb7d04b97e6256c19
ammbra <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>
2022-03-22 12:17:10 +0100

    removed unused import

--- src/main/java/com/redhat/developers/HobbyResource.java
+++ src/main/java/com/redhat/developers/HobbyResource.java
@@ -5,7 +5,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-import javax.inject.Inject;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
